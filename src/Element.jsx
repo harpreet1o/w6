058c41942b499904842dc0ece3234c48228a9d0c
Harpreet Singh <harpreetx01@outlook.com>
2024-06-22 11:46:32 -0700

    completed

--- src/Element.jsx
+++ src/Element.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 function Element(){
+    const [data,setData]=useState(null);
    async function fetching(){
     const res=  await fetch(`https://dog.ceo/api/breeds/list/all`);
-    let data=await res.json();
-    console.log(data);
+   let rec=await res.json();
+   console.log(rec);
+   setData(rec);
+   
       
     }
     return (
         <div>
         <button onClick={()=>fetching()}>Click Me</button>
+        {
+            data&&<p>Data received loaded in console</p>
+        }
         </div>
     )
 }
